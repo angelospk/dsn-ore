baf5e2b690b5b0038c3b58e544faf2697c1f9223
angelospk <rovas450@gmail.com>
2025-11-26 18:42:41 +0200

    feat: Externalize site configuration, add a breadcrumb component, and remove header and footer components.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,11 @@ import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+import { readFileSync } from 'fs';
+const siteConfig = JSON.parse(readFileSync(new URL('./src/site.config.json', import.meta.url), 'utf-8'));
+
 export default defineConfig({
-	site: 'https://angelospk.github.io',
-	base: '/dsn-ore',
+	site: siteConfig.site,
+	base: siteConfig.base,
 	integrations: [mdx(), sitemap()],
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
+import config from './site.config.json';
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Δημοτικό Συμβούλιο Νέων Ορεστιάδας';
+export const SITE_DESCRIPTION = 'Σελίδα Ενημέρωσης για το Δημοτικό Συμβούλιο Νέων Ορεστιάδας';
+export const SITE_URL = config.site;
+export const BASE_URL = config.base;
